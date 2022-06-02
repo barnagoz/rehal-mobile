2557c016b811a36fa8b3329ed38caee7d21b09e8
Barnabás Gőz <goozbarnabas@gmail.com>
2022-06-02 19:59:49 +0200

    init

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+// screens
+import Szabalyok from "./screens/szabalyok.js";
+import Bekuldes from "./screens/bekuldes.js";
+import Idozito from "./screens/idozito.js";
 
 export default function App() {
+  const Tab = createBottomTabNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={{ headerShown: false }} initialRouteName="Játékszabályok">
+        <Tab.Screen
+          name="Játékszabályok"
+          component={Szabalyok}
+          options={{
+            tabBarLabel: "Játékszabályok",
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="format-list-bulleted" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Beküldés"
+          component={Bekuldes}
+          options={{
+            tabBarLabel: "Beküldés",
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="plus-circle-outline" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Időzítő"
+          component={Idozito}
+          options={{
+            tabBarLabel: "Időzítő",
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="timer" color={color} size={size} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/bekuldes.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {View, Text} from 'react-native'
+
+export default function Bekuldes() {
+  return (
+      <View>
+          <Text>Bekuldes</Text>
+      </View>
+  )
+}
